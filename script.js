faf71c573d1bab2c81442a693031d926f208300a
Klaudia Waclawska-Cwiertnia <kaywoo@o2.pl>
2019-10-07 21:23:39 +0200

    files added

---
+++ script.js
@@ -0,0 +1,15 @@
+let number = 1;
+const btn = document.querySelector("button");
+const list = document.querySelector("ul");
+
+btn.addEventListener("click", function () {
+  const li = document.createElement("li");
+  li.textContent = number;
+
+  list.appendChild(li);
+
+  if (number % 3 == 0) {
+    li.classList.add("big");
+  }
+  number += 2;
+});
